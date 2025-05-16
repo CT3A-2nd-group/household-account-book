8cb913cca49b4977398e8e831989d5b251daf8e7
Takiharu19 <taki41836969@gmail.com>
2025-05-16 04:40:27 +0000

    グラフ表示の調整（仮）

--- views/graph.php
+++ views/graph.php
@@ -11,53 +11,52 @@ include __DIR__ . '/layouts/header.php'; // 必要に応じて変更
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
-fetch('/graph')
-  .then(response => response.json())
-  .then(json => {
-    const ctx = document.getElementById('myLineChart').getContext('2d');
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: json.labels,
-        datasets: [{
-          label: '収入額',
-          data: json.data,
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          borderColor: 'rgba(75, 192, 192, 1)',
-          borderWidth: 2,
-          fill: true,
-          tension: 0.1
-        }]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          y: {
-            beginAtZero: true,
-            min:0,
-            max:200000,
-            ticks:{
-                stepSize: 20000,
-                callback: value => value + '円'
+    fetch('/incomeGraph')
+    .then(response => response.json())
+    .then(json => {
+        const ctx = document.getElementById('myLineChart').getContext('2d');
+        new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: json.labels,
+            datasets: [{
+            label: '収入額',
+            data: json.data,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 2,
+            fill: true,
+            tension: 0.1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+            y: {
+                beginAtZero: true,
+                max: json.max,
+                min:0,
+                ticks:{
+                    callback: value => value + '円'
+                },
+                title: {
+                display: true,
+                text: '金額（円）'
+                }
             },
-            title: {
-              display: true,
-              text: '金額（円）'
+            x: {
+                title: {
+                display: true,
+                text: '日付'
+                }
             }
-          },
-          x: {
-            title: {
-              display: true,
-              text: '日付'
             }
-          }
         }
-      }
+        });
+    })
+    .catch(error => {
+        console.error('データの取得に失敗しました:', error);
     });
-  })
-  .catch(error => {
-    console.error('データの取得に失敗しました:', error);
-  });
 </script>
 
 <?php include __DIR__ . '/layouts/footer.php'; ?>

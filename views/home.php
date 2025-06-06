38769ecb0c843d835619bba35f136f2d477f9f55
Takiharu19 <taki41836969@gmail.com>
2025-06-06 04:45:34 +0000

    あなたは○○ですを削除

--- views/home.php
+++ views/home.php
@@ -4,11 +4,6 @@
     <section class="welcome-section">
         <div class="user-greeting">
             <h2 class="greeting-text">ようこそ、<?= htmlspecialchars($username) ?> さん！</h2>
-            <?php if (!empty($isAdmin)): ?>
-                <p class="user-role admin-role">あなたは<strong>管理者</strong>です。</p>
-            <?php else: ?>
-                <p class="user-role user-role-standard">あなたは一般ユーザーです。</p>
-            <?php endif; ?>
         </div>
     </section>
 
